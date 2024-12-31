4e262d69baaa5c74933346ad4c13b787a7abd898
Alicia Kee <155275698+alicia-kee@users.noreply.github.com>
2024-12-31 12:07:04 -0600

    Add slider for size of grid

--- javascript.js
+++ javascript.js
@@ -1,24 +1,27 @@
 const grid = document.querySelector(".grid-container");
-let currSize = 30; //for now
-let colour = "black";
+let currSize = 20; //default
+let colour = "black"; //default
 
-function createGrid(size){ //size is width/height of each box
-    if(size>100){
-        size = 100; //default max size
+function createGrid(){ //size is width/height of each box
+    if(currSize>100){
+        currSize = 100; //default max size
     }
+
+    sizeText.textContent = currSize + " x " + currSize;
+
     grid.innerHTML = ""; //clear grid
-    const padding = ((500/size)/2) - 0.5;
+    const padding = ((500/currSize)/2) - 0.5;
     //500 is height and width of container, later create var. 
     //0.5 is border of each box
 
     const paddingStr = padding + "px";
 
-    for (let i=0; i<size; i++){
+    for (let i=0; i<currSize; i++){
         const rowContainer = document.createElement('div');
         rowContainer.classList.add("row-container"); //each newDiv created has this class
         grid.appendChild(rowContainer);
 
-        for (let j=0; j<size; j++){
+        for (let j=0; j<currSize; j++){
             const gridElement = document.createElement("div");
             gridElement.classList.add("grid-square");
             gridElement.style.padding = paddingStr;
@@ -56,6 +59,13 @@ function switchToColour(){
     eraserBtn.style.backgroundColor = "#5e5e5e";
 }
 
+function changeSize(){
+    currSize = sizePicker.value;
+    createGrid();
+
+
+}
+
 const clearBtn = document.querySelector(".clear-btn");
 clearBtn.addEventListener("click", clearGrid)
 
@@ -67,5 +77,9 @@ eraserBtn.addEventListener("click", switchToEraser);
 
 const colourBtn = document.querySelector(".colour-btn");
 colourBtn.addEventListener("click", switchToColour);
+
+const sizePicker = document.querySelector(".slider");
+sizePicker.addEventListener("input", changeSize);
+const sizeText = document.querySelector(".slider-value");
 
 createGrid(currSize);
